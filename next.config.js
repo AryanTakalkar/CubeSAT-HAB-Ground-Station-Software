1ac6e835657f28c1d6ce8e4baa6e7d960b067f89
Aryan Takalkar <aryantakalkar1365@gmail.com>
2026-06-19 00:00:14 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false, net: false, tls: false,
+      };
+    }
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'faye-websocket': false,
+    };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin-allow-popups' },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
